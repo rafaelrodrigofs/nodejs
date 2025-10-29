e59f5e522b88182dbb7c9f7ba9f51ac6abd58412
Rafael Rodrigo <rafaelrodrigoartes@gmail.com>
2025-10-29 08:34:16 -0300

    Inserindo e realizando busca dos dados no banco de dados

--- aula03/criarTabela.sql
+++ aula03/criarTabela.sql
@@ -8,3 +8,41 @@ CREATE TABLE usuario(
 
 ## Verificar se a tabela foi criada
 SHOW TABLES;
+
+## Inserir dados na tabela usuario
+INSERT INTO usuario (
+    nome,
+    email,
+    idade
+) VALUES (
+    'João','joao@example.com',20,
+    'Maria','maria@example.com',25,
+    'Pedro','pedro@example.com',30,
+    'Ana','ana@example.com',22,
+    'Carlos','carlos@example.com',28,
+    'Laura','laura@example.com',23,
+    'Pedro','pedro@example.com',30,
+    'Ana','ana@example.com',22,
+    'Carlos','carlos@example.com',28,
+    'Laura','laura@example.com',23,
+);
+
+## Selecionar todos os dados da tabela usuario
+SELECT * FROM usuario;
+
+## Exemplos de busca na tabela usuario:
+
+-- 1. Buscar todos os usuários com idade maior ou igual a 25 anos
+SELECT nome FROM usuario WHERE idade >= 25;
+
+-- 2. Buscar usuários com nome igual a 'Lucas'
+SELECT nome FROM usuario WHERE nome = 'Lucas';
+
+-- 3. Buscar usuários com email terminando em '@example.com'
+SELECT nome, email FROM usuario WHERE email LIKE '%@example.com';
+
+-- 4. Buscar usuários com idade entre 22 e 28 anos
+SELECT nome, idade FROM usuario WHERE idade BETWEEN 22 AND 28;
+
+-- 5. Buscar usuários cujo nome comece com a letra 'A'
+SELECT nome FROM usuario WHERE nome LIKE 'A%';
